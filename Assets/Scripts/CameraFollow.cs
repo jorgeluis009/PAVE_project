e235c8baa2e7377ba632598f485624d2d42c0f5e
PerseusPolis <perseus@polispay.org>
2019-05-20 18:18:33 -0500

    hola

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;            // The position that that camera will be following.
+        public float smoothing = 5f;        // The speed with which the camera will be following.
+
+        Vector3 offset;                     // The initial offset from the target.
+
+        void Start()
+        {
+            offset = transform.position - target.position;
+        }
+
+        void FixedUpdate()
+        {
+            
+            if (target.position != null)
+            {
+                Vector3 targetCamPos = target.position + offset;
+                transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+            }
+        }
+
+        public void setTarget(Transform newTarget)
+        {
+            target = newTarget;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/click2Move.cs
@@ -0,0 +1,52 @@
+﻿using CompleteProject;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+public class click2Move : NetworkBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent agent;
+    
+    private bool isMoving = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+        agent = GetComponent<NavMeshAgent>();    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            isMoving = false;
+        }
+        else
+        {
+            isMoving = true;
+        }
+        // animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isWalking", isMoving);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        Camera.main.GetComponent<CameraFollow>().setTarget(gameObject.transform);
+    }
+}
